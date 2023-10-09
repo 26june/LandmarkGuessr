1e8db0743f5a8b4e8d176f07757834ad8d18e446
26june <gj.abordo1@gmail.com>
2023-10-09 14:21:48 +0100

    start image logic

--- js/game.js
+++ js/game.js
@@ -4,8 +4,13 @@ const buttonContainer = document.getElementById("buttonContainer");
 const startButton = document.getElementById("startButton");
 const submitButton = document.getElementById("submitButton");
 
-function handleStartButton(e) {
+const answerForm = document.getElementsByTagName("form");
+
+function handleStartButton() {
   imgContainer.removeChild(buttonContainer);
+  answerForm[0][0].disabled = false;
+  answerForm[0][1].disabled = false;
+
   anime({
     targets: "#france",
     easing: "easeInOutExpo",
@@ -13,10 +18,10 @@ function handleStartButton(e) {
       anim.animatables[0].target.style.filter = `blur(${
         ((anim.progress - 100) * -1) / 100
       }px)`;
-
-      console.log(anim.animatables[0].target.style.filter);
     },
   });
+
+  submitButton.addEventListener("submit", handleAnswer);
 }
 
 let tries = 0;
@@ -25,17 +30,21 @@ const scaling = [10, 5, 3, 2, 1];
 function handleAnswer(e) {
   e.preventDefault();
   if (e.target[0].value === "france") {
-    console.log("correct answer");
     zoomOutImage(scaling[4]);
+    correctAnswer();
+    console.log("correct answer");
   } else if (tries === 4) {
     zoomOutImage(scaling[4]);
     console.log("You have no more tries, the correct answer was france");
   } else {
     e.target[0].value = ""; //clear the text box after the answer
+    shakeForm();
     zoomOutImage(scaling[tries]); //zoom out image based on tries
     tries++;
   }
 }
+
+//Animations
 
 function zoomOutImage(scaling) {
   anime({
@@ -46,5 +55,34 @@ function zoomOutImage(scaling) {
   });
 }
 
+function shakeForm() {
+  const moveX = 8;
+
+  anime({
+    targets: "body",
+    easing: "easeInOutSine",
+    translateX: [moveX, -moveX, moveX / 2, -moveX / 2, 0],
+  });
+
+  wrongAnswer();
+}
+
+function wrongAnswer() {
+  anime({
+    targets: "#imgContainer",
+    easing: "linear",
+    duration: 500,
+    borderColor: ["rgba(255, 0, 0, 0)", "rgba(255, 0, 0, 0.7)"],
+  });
+}
+
+function correctAnswer() {
+  anime({
+    targets: "#imgContainer",
+    easing: "linear",
+    duration: 500,
+    borderColor: ["rgba(255, 0, 0, 0)", "rgba(0, 255, 0, 0.7)"],
+  });
+}
+
 startButton.addEventListener("click", handleStartButton);
-submitButton.addEventListener("submit", handleAnswer);
