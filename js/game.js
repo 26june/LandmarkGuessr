bdccfe9fbcfa317392d523f80ec127e23fb3bd73
26june <gj.abordo1@gmail.com>
2023-10-09 15:43:44 +0100

    add images as objects

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+// Get elements
 const imgContainer = document.getElementById("imgContainer");
 const buttonContainer = document.getElementById("buttonContainer");
 
@@ -6,20 +7,55 @@ const submitButton = document.getElementById("submitButton");
 
 const answerForm = document.getElementsByTagName("form");
 
+const imgPlaceholder = document.getElementsByTagName("img");
+
+//Globals
+let currentIndex;
+
+let animationImageTarget;
+
+function Landmarks(landmarkName, landmarkSrc) {
+  this.landmarkName = landmarkName;
+  this.landmarkSrc = landmarkSrc;
+
+  allLandmarks.push(this);
+}
+
+const allLandmarks = [];
+
+function generateRandom() {
+  return Math.floor(Math.random() * allLandmarks.length);
+}
+
+function startPageLoad() {
+  new Landmarks("France", "../assets/France.jpg");
+  new Landmarks("Japan", "../assets/Japan.jpg");
+  new Landmarks("Egypt", "../assets/Egypt.jpg");
+  new Landmarks("Brazil", "../assets/Brazil.jpg");
+  new Landmarks("Philippines", "../assets/Philippines.jpg");
+  new Landmarks("Greece", "../assets/Greece.jpg");
+  new Landmarks("Italy", "../assets/Italy.jpg");
+  new Landmarks("Ukraine", "../assets/Ukraine.jpg");
+
+  currentIndex = generateRandom();
+
+  imgPlaceholder[0].src = allLandmarks[currentIndex].landmarkSrc;
+  imgPlaceholder[0].id = imgPlaceholder[0].alt =
+    allLandmarks[currentIndex].landmarkName;
+
+  animationImageTarget = `#${allLandmarks[currentIndex].landmarkName}`;
+
+  console.dir(imgPlaceholder[0]);
+}
+
+startPageLoad();
+
 function handleStartButton() {
   imgContainer.removeChild(buttonContainer);
   answerForm[0][0].disabled = false;
   answerForm[0][1].disabled = false;
 
-  anime({
-    targets: "#france",
-    easing: "easeInOutExpo",
-    update: function (anim) {
-      anim.animatables[0].target.style.filter = `blur(${
-        ((anim.progress - 100) * -1) / 100
-      }px)`;
-    },
-  });
+  removeBlur();
 
   submitButton.addEventListener("submit", handleAnswer);
 }
@@ -29,7 +65,10 @@ const scaling = [10, 5, 3, 2, 1];
 
 function handleAnswer(e) {
   e.preventDefault();
-  if (e.target[0].value.toLowerCase() === "france") {
+  if (
+    e.target[0].value.toLowerCase() ===
+    allLandmarks[currentIndex].landmarkName.toLowerCase()
+  ) {
     zoomOutImage(scaling[4]);
     correctAnswer();
     console.log("correct answer");
@@ -45,11 +84,26 @@ function handleAnswer(e) {
   }
 }
 
+//Event listeners
+startButton.addEventListener("click", handleStartButton);
+
 //Animations
+
+function removeBlur() {
+  anime({
+    targets: animationImageTarget,
+    easing: "easeInOutExpo",
+    update: function (anim) {
+      anim.animatables[0].target.style.filter = `blur(${
+        ((anim.progress - 100) * -1) / 100
+      }px)`;
+    },
+  });
+}
 
 function zoomOutImage(scaling) {
   anime({
-    targets: "#france",
+    targets: animationImageTarget,
     scale: scaling / 20,
     easing: "easeInOutExpo",
     duration: 2500,
@@ -57,7 +111,7 @@ function zoomOutImage(scaling) {
 }
 
 function shakeForm() {
-  const moveX = 8;
+  const moveX = 32;
 
   anime({
     targets: "body",
@@ -94,5 +148,3 @@ function correctAnswer() {
     borderColor: ["rgba(255, 0, 0, 0)", "rgba(140, 212, 126, 1)"],
   });
 }
-
-startButton.addEventListener("click", handleStartButton);
