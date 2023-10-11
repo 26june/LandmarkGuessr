96615153655823dda28561516ec82abc06b7acf0
26june <gj.abordo1@gmail.com>
2023-10-11 15:55:50 +0100

    reset back to previous commit

--- js/app.js
+++ js/app.js
@@ -1,65 +1,32 @@
-// const nameForm = document.getElementById("nameForm");
-// const messageDisplay = document.getElementById("message");
-// const loadingDiv = document.getElementById("loading");
+const nameForm = document.getElementById("nameForm");
+const messageDisplay = document.getElementById("message");
+const loadingDiv = document.getElementById("loading");
 
-// const storedNames = localStorage.getItem("user_name");
+const storedNames = localStorage.getItem("user_name");
 
-// if (storedNames) {
-//   messageDisplay.textContent = `Welcome, ${storedNames}!`;
-// }
-
-// nameForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-
-//   const nameInput = document.getElementById("name");
-//   const userName = nameInput.value;
-
-//   localStorage.setItem("user_name", userName);
-
-//   messageDisplay.textContent = `Welcome, ${userName}!. Ready to Play`;
-
-//   // Hide the name form and show the loading div
-//   nameForm.style.display = "none";
-//   loadingDiv.style.display = "block";
-
-//   // Simulate a 3-second delay and then redirect to the game page
-//   setTimeout(function () {
-//     window.location.href = "game.html";
-//   }, 3000);
-
-//   // nameInput.value = "";
-// });
-
-let moveY = 16;
-
-let mapPinsId = {
-  home: "#homePin",
-  game: "#gamePin",
-  leaderboard: "#leaderboardPin",
-};
-
-let mapPinsHref = {
-  home: "../index.html",
-  game: "../game.html",
-  leaderboard: "../scoreboard.html",
-};
-
-function hoverPoint(pins) {
-  anime({
-    targets: mapPinsId[pins],
-    easing: "easeInOutSine",
-    translateY: [-moveY, 0, -moveY / 2, 0],
-  });
+if (storedNames) {
+  messageDisplay.textContent = `Welcome, ${storedNames}!`;
 }
 
-function clickPoint(clickLocation) {
-  anime({
-    targets: mapPinsId[clickLocation],
-    easing: "easeInOutSine",
-    duration: 1000,
-  });
+nameForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-  setTimeout(() => {
-    window.location.href = mapPinsHref[clickLocation];
-  }, 0);
-}
+  const nameInput = document.getElementById("name");
+  const userName = nameInput.value;
+
+  localStorage.setItem("user_name", userName);
+
+  messageDisplay.textContent = `Welcome, ${userName}!. Ready to Play`;
+
+  // Hide the name form and show the loading div
+  nameForm.style.display = "none";
+  loadingDiv.style.display = "block";
+
+  // Simulate a 3-second delay and then redirect to the game page
+  setTimeout(function () {
+    window.location.href = "game.html";
+  }, 3000);
+
+  // nameInput.value = "";
+});
+
